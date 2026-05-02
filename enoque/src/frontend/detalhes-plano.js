e1cb49ccf067bbdc9517644e10e9cf4e0b75a464
jraimundonetto-crypto <jraimundonetto@gmail.com>
2026-05-02 04:11:55 -0300

    Delete enoque/src directory

--- enoque/src/frontend/detalhes-plano.js
+++
@@ -1,9 +0,0 @@
-const dados = JSON.parse(localStorage.getItem('plano_modalidade'));
-
-document.getElementById('titulo-plano').innerText = dados.modalidade;
-
-document.getElementById('detalhes-texto').innerText = `
-Durante esta semana, foque em fortalecer ${dados.modalidade}.
-Defina metas claras, acompanhe resultados diariamente e execute ações práticas alinhadas ao seu momento atual.
-A prioridade agora é aumentar consistência, corrigir falhas e gerar evolução perceptível.
-`;
